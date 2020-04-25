ef35503e1d4605c6db1b95cd3459a1b1a938fe6c
Oscar Amado <oscarfamado@gmail.com>
2020-04-25 18:35:45 -0500

    interval

---
+++ src/observables/05-interval.ts
@@ -0,0 +1,16 @@
+import {interval, timer} from 'rxjs';
+
+
+const observer = {
+    next: val => console.log('next', val),
+    complete: () => console.log('complete')
+};
+
+// Intervalo asincrono.
+const interval$ = interval(1000);
+const timer$ = timer(2000);
+
+console.log('inicio');
+interval$.subscribe(observer);
+timer$.subscribe(observer);
+console.log('fin');

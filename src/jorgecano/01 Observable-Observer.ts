accddcd78253123f7db960a9da5f20eda69cecc1
ofaaoficial <oscarfamado@gmail.com>
2020-12-11 18:55:57 -0500

    observables observer

---
+++ src/jorgecano/01 Observable-Observer.ts
@@ -0,0 +1,37 @@
+import {Observable, Observer} from 'rxjs';
+import {filter, map} from "rxjs/operators";
+import {isNumeric} from "rxjs/internal-compatibility";
+
+
+const myObserver: Observer<any> = {
+    next(x: number) {
+        if (isNumeric(x)) {
+            console.log(x + 10);
+        } else {
+            console.log('No es un numero')
+        }
+    },
+    error(err) {
+        console.error('[Error] => ', err);
+    },
+    complete() {
+        console.log('Trabajo terminado')
+    }
+}
+
+const myObservable = new Observable(subscriber => {
+    subscriber.next('observable 1');
+    subscriber.next('observable 2');
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    subscriber.error('error from observable');
+});
+
+const myObservable2 = new Observable(subscriber => {
+    subscriber.complete();
+});
+
+myObservable.subscribe(myObserver);
+myObservable2.subscribe(myObserver);
+

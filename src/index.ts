93daa9d74599847f5557698648f76eafe2f4b423
ofaaoficial <oscarfamado@gmail.com>
2020-12-15 22:57:30 -0500

    subject example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log('...');
+import {Observable, Subject} from "rxjs";
+
+const API_URL = 'https://rickandmortyapi.com/api/character/1';
+
+
+const sub$ = new Subject();
+
+
+
+sub$.subscribe((data) => {
+    console.log('[1] data', data)
+})
+
+sub$.subscribe((data) => {
+    console.log('[2] data', data)
+})
+
+sub$.next(5);
+sub$.next(2);
+sub$.next(5);
+sub$.next(2);
+sub$.next(5);
+sub$.next(2);
+sub$.next(5);
+sub$.next(2);
+
+
+
+// const ob$ = new Observable((subscriber) => {
+//     subscriber.next(1);
+//     subscriber.next(2);
+// });
+//
+// ob$.next()
+//
+// ob$.subscribe(response => {
+//     console.log('response', response)
+// })
